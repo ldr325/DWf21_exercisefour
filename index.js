d729d2b83a090b85a7398ea935e374b487952c43
LeoR <ldr325@nyu.edu>
2021-10-28 08:45:28 -0400

    form and routes

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 const indexRoute = require("./routes/index");
+const articleRoute = require("./routes/article");
+const createArticleRoute = require("./routes/createArticle");
 
 app.use("/", indexRoute);
+app.use("/article", articleRoute);
+app.use("/create", createArticleRoute);
 
 app.listen(port, () => {
-  console.log("example app listening at http://localhost:4000");
+  console.log("example app listening at http://localhost:${port}");
 });
--- routes/article.js
+++ routes/article.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send(`
-    <h1>All Articles</h1>
-    <p>Articles coming soon... need to set up firebase</p>`);
+    <h1>Individual Article</h1>
+    <p>A single post will go here...</p>`);
 });
 
 module.exports = router;
